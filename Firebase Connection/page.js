ef5fa00a86055e431c6e074a19e45ede9da2b9da
Narmi002 <shaiknarmi002@gmail.com>
2024-07-10 13:04:13 +0530

    authentication

---
+++ Firebase Connection/page.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+// import { getAnalytics, isSupported } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey:process.env.API_KEY,
+    authDomain:process.env.AUTH_DOMAIN ,
+    projectId:process.env.PROJECT_ID,
+    storageBucket:process.env.STORAGE_BUCKET,
+    messagingSenderId:"506855800201",
+    appId: process.env.APP_ID,
+    measurementId: "G-GQGNHXP41R"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  // const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
+
+  const db=getFirestore(app)
+
+  export  {db}
--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,16 +1,45 @@
 import NextAuth from 'next-auth'
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
-import GitHubProvider from 'next-auth/providers/github'
+import { db } from '@/Firebase Connection/page'
+import { setDoc, doc, addDoc, collection, getDocs, query, where } from 'firebase/firestore'
 
-export const auth=NextAuth({
-    providers:[
+
+export const auth = NextAuth({
+    providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET
-          })
-    ]
+        })
+    ],
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            try {
+                if (account.provider == 'google') {
+                    const user_db = collection(db, 'login')
+                    const check_user = query(user_db, where('email', '==', user.email))
+                    const get_user = await getDocs(check_user)
+                    if (get_user.empty) {
+                        const save_data = await addDoc(user_db, {
+                            name: user.name,
+                            email: user.email,
+                            image: user.image
+                        })
+                    }
+                    else {
+                        console.log("user already exists")
+                    }
+                }
+
+            } catch (error) {
+                console.error(error)
+            }
+            return true
+
+        }
+
+    }
 })
+
+
 
 export { auth as GET, auth as POST }
