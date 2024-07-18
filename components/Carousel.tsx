1194999132952055d287dd91285eb36ace014a4d
AST <ashwajittayade30@gmail.com>
2024-07-18 22:15:28 +0530

    Changed Caption for testing

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -2,14 +2,14 @@
 
 import React from 'react';
 import { Box, IconButton, useBreakpointValue } from '@chakra-ui/react';
-// Here we have used react-icons package for the icons
+
 import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
-// And react-slick as our Carousel Lib
+
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-// Settings for the slider
+
 const settings = {
   dots: true,
   arrows: false,
@@ -23,12 +23,9 @@ const settings = {
 };
 
 export default function Carousel() {
-  // As we have used custom buttons, we need a reference variable to
-  // change the state
+
   const [slider, setSlider] = React.useState<Slider | null>(null);
 
-  // These are the breakpoints which changes the position of the
-  // buttons as the screen size changes
   const top = useBreakpointValue({ base: '90%', md: '50%' });
   const side = useBreakpointValue({ base: '30%', md: '10px' });
 
@@ -89,3 +86,6 @@ export default function Carousel() {
     </Box>
   );
 }
+
+
+//Temp Caption for testing
