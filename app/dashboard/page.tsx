5736b43d2dae1f69c33f50cea7e3de440693a265
AST <ashwajittayade30@gmail.com>
2024-10-28 18:35:51 +0530

    pds and spd doc

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,43 +1,14 @@
 "use client";
 
 import {
-  IconButton,
-  Avatar,
   Box,
-  CloseButton,
-  Flex,
   HStack,
   VStack,
   Icon,
   useColorModeValue,
   Text,
-  Drawer,
-  DrawerContent,
-  useDisclosure,
-  BoxProps,
-  FlexProps,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-  useColorMode,
-  Image,
   Heading,
-  Stack,
 } from "@chakra-ui/react";
-import {
-
-  // FiTrendingUp,
-  FiCompass,
-  // FiStar,
-  FiSettings,
- 
-  FiUpload,
-
-} from "react-icons/fi";
-import { IconType } from "react-icons";
-import { FiHome, FiUser, FiChevronDown, FiMenu, FiBell, FiDownload, FiBriefcase, FiBookOpen } from "react-icons/fi"; // Imported required icons
 
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import useGetTeam from "../../utils/useGetTeam";
@@ -48,208 +19,21 @@ import { usePresence, motion } from "framer-motion";
 import { useRef, useEffect, useState } from "react";
 import { signOut, useSession } from "next-auth/react";
 import gsap from "gsap";
-import { TbListDetails } from "react-icons/tb";
 
-interface LinkItemProps {
-  name: string;
-  icon: IconType;
-  url?: string;
-}
 
-interface NavItemProps extends FlexProps {
-  icon: IconType;
-  children: React.ReactNode;
-}
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../../firebase/config";
 
-interface MobileProps extends FlexProps {
-  headName: string;
-  onOpen: () => void;
-}
+    interface User {
+        id: string;
+        email: string;
+        uid: string;
+        teamname?: string;
+        designfile?: string;
+        demonstrationfile?: string;
+    }
 
-interface SidebarProps extends BoxProps {
-  onClose: () => void;
-}
 
-const LinkItems: LinkItemProps[] = [
-  { name: "Home", icon: FiHome, url: "/" },
-  { name: "Team Details", icon: FiUser, url: "/dashboard" },
-  { name: "Demonstration", icon: FiDownload, url: "/dashboard/submission1" },
-  { name: "DesignX Blueprint", icon: FiBriefcase, url: "/dashboard/submission2" },
-  { name: "Hyperloop Innoquest", icon: FiBookOpen, url: "/dashboard/submission3" },
-];
-
-const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
-  return (
-    <Box
-      transition="3s ease"
-      bg={useColorModeValue("white", "gray.900")}
-      borderRight="1px"
-      borderRightColor={useColorModeValue("gray.200", "gray.700")}
-      w={{ base: "full", md: 60 }}
-      pos="fixed"
-      h="full"
-      {...rest}
-    >
-      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        <Link href="/">
-          <Image
-            src={useColorModeValue("/GHC-LOGO-BLACK.png", "/GHC-logo.png")}
-            h={6}
-            alt="GHC_logo"
-          />
-        </Link>
-        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
-      </Flex>
-      {LinkItems.map((link) => (
-        <Link key={link.name} href={link.url || "/"}>
-          <NavItem icon={link.icon}>{link.name}</NavItem>
-        </Link>
-      ))}
-    </Box>
-  );
-};
-
-const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
-  return (
-    <Box style={{ textDecoration: "none" }} _focus={{ boxShadow: "none" }}>
-      <Flex
-        align="center"
-        p="4"
-        mx="4"
-        borderRadius="lg"
-        role="group"
-        cursor="pointer"
-        _hover={{
-          bg: "red.500",
-          color: "white",
-        }}
-        {...rest}
-      >
-        {icon && (
-          <Icon
-            mr="4"
-            fontSize="16"
-            _groupHover={{
-              color: "white",
-            }}
-            as={icon}
-          />
-        )}
-        {children}
-      </Flex>
-    </Box>
-  );
-};
-
-const MobileNav = ({ onOpen, headName, ...rest }: MobileProps) => {
-  const navigate = useRouter();
-  const { colorMode, toggleColorMode } = useColorMode();
-  const [team, isLoading] = useGetTeam();
-  console.log(team);
-
-  function handleLogout() {
-    signOut({ redirect: false });
-    navigate.push("/");
-  }
-
-  return (
-    <Flex
-      ml={{ base: 0, md: 60 }}
-      px={{ base: 4, md: 4 }}
-      height="20"
-      alignItems="center"
-      bg={useColorModeValue("white", "gray.900")}
-      borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
-      justifyContent={{ base: "space-between", md: "flex-end" }}
-      {...rest}
-    >
-      <IconButton
-        display={{ base: "flex", md: "none" }}
-        onClick={onOpen}
-        variant="outline"
-        aria-label="open menu"
-        icon={<FiMenu />}
-      />
-      <Link href="/">
-        {/* <Text
-                    display={{ base: 'flex', md: 'none' }}
-                    fontSize="2xl"
-                    fontFamily="monospace"
-                    fontWeight="bold">
-                    GHC
-                </Text> */}
-        <Image
-          display={{ base: "flex", md: "none" }}
-          src={useColorModeValue("/GHC-LOGO-BLACK.png", "/GHC-logo.png")}
-          h={6}
-          alt="GHC_Logo"
-        />
-      </Link>
-
-      <HStack spacing={{ base: "0", md: "6" }}>
-        <IconButton
-          size="lg"
-          variant="ghost"
-          aria-label="open menu"
-          icon={<FiBell />}
-        />
-        <IconButton
-          size="lg"
-          variant="ghost"
-          aria-label="open menu"
-          onClick={toggleColorMode}
-          icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-        />
-        <Flex alignItems={"center"}>
-          {isLoading ? (
-            <>...</>
-          ) : (
-            <Menu>
-              <MenuButton
-                py={2}
-                transition="all 0.3s"
-                _focus={{ boxShadow: "none" }}
-              >
-                <HStack>
-                  <Avatar size={"sm"} src={team?.profilePictureUrl!} />
-                  <VStack
-                    display={{ base: "none", md: "flex" }}
-                    alignItems="flex-start"
-                    spacing="1px"
-                    ml="2"
-                  >
-                    <Text fontSize="sm">{headName}</Text>
-                    <Text fontSize="xs" color="gray.600">
-                      Team Rep
-                    </Text>
-                  </VStack>
-                  <Box display={{ base: "none", md: "flex" }}>
-                    <FiChevronDown />
-                  </Box>
-                </HStack>
-              </MenuButton>
-              <MenuList
-              // bg={useColorModeValue('white', 'gray.900')}
-              // borderColor={useColorModeValue('gray.200', 'gray.700')}
-              >
-                {/* <Link href={"/dashboard/profile"}>
-                  <MenuItem>Profile</MenuItem>
-                </Link> */}
-                {/* <Link href={"/dashboard/settings"}>
-                  <MenuItem>Settings</MenuItem>
-                </Link> */}
-
-                <MenuDivider />
-                <MenuItem onClick={handleLogout}>Sign out</MenuItem>
-              </MenuList>
-            </Menu>
-          )}
-        </Flex>
-      </HStack>
-    </Flex>
-  );
-};
 
 export const Loading: React.FC<any> = (props: any) => {
   const ref = useRef(null);
@@ -326,53 +110,87 @@ const Detail = ({ label, value }: { label: string; value: string }) => {
 };
 
 const SidebarWithHeader = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const [team, isLoading] = useGetTeam();
   const [loading, setLoading] = useState<boolean>(true);
-  const cardBgColor = useColorModeValue("white", "gray.800");
+  const [entries, setEntries] = useState<User[]>([]);
 
+
+
+  const groupUsers = (fetchedUsers: User[]) => {
+    const groupedUsers: { [uid: string]: User } = {};
+
+    fetchedUsers.forEach((user) => {
+        if (!groupedUsers[user.uid]) {
+            // Initialize with the first user doc for each uid
+            groupedUsers[user.uid] = { ...user };
+        } else {
+            // Merge designfile and demonstrationfile if they exist
+            groupedUsers[user.uid] = {
+                ...groupedUsers[user.uid],
+                designfile: user.designfile || groupedUsers[user.uid].designfile,
+                demonstrationfile: user.demonstrationfile || groupedUsers[user.uid].demonstrationfile,
+            };
+        }
+    });
+
+    return Object.values(groupedUsers);
+};
+
+useEffect(() => {
+  const usersCollection = collection(db, "submissions");
+
+  const unsubscribe = onSnapshot(usersCollection, (snapshot) => {
+      const fetchedUsers = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+      })) as User[];
+
+      const groupedUsers = groupUsers(fetchedUsers);
+      setEntries(groupedUsers);
+  });
+
+  return () => unsubscribe();
+}, []);
+
+  console.log(team);
+  console.log(entries);
   useEffect(() => {
     setLoading(isLoading);
+    
   }, [isLoading]);
 
   return (
-    <Box  bg={useColorModeValue("gray.100", "gray.900")}>
-     
-            {/* Content */}
-            <VStack
-              spacing={8}
-              py={8}
-              px={16}
-              
-              w={"fit-content"}
-              borderRadius={"lg"}
-              textAlign={"left"}
-            >
-              <Heading fontSize={{ base: "2xl", md: "5xl" }}>
-                {team?.teamname}
-              </Heading>
+    <Box bg={useColorModeValue("gray.100", "gray.900")}>
+      {/* Content */}
+      <VStack
+        spacing={8}
+        py={8}
+        px={16}
+        w={"fit-content"}
+        borderRadius={"lg"}
+        textAlign={"left"}
+      >
+        <Heading fontSize={{ base: "2xl", md: "5xl" }}>
+          {team?.teamname}
+        </Heading>
 
-              <VStack textAlign={"left"} w={"full"} alignItems={"baseline"}>
-                <Detail label="University" value={team?.homeUniversity} />
-                <Detail label="Active members" value={team?.activemembers} />
-                <Detail
-                  label="Members attending event"
-                  value={team?.attendeventmembers}
-                />
-                <Detail
-                  label="Representative"
-                  value={team?.teamrepresentetive}
-                />
-                <Detail
-                  label="Representative Email"
-                  value={team?.emailrepresentetive}
-                />
-                <Detail label="WhatsApp Number" value={team?.phone} />
-                <Detail label="Country" value={team?.country} />
-                <Detail label="Postal Code" value={team?.postalcode} />
-              </VStack>
-            </VStack>
-   
+        <VStack textAlign={"left"} w={"full"} alignItems={"baseline"}>
+          <Detail label="University" value={team?.homeUniversity} />
+          <Detail label="Active members" value={team?.activemembers} />
+          <Detail
+            label="Members attending event"
+            value={team?.attendeventmembers}
+          />
+          <Detail label="Representative" value={team?.teamrepresentetive} />
+          <Detail
+            label="Representative Email"
+            value={team?.emailrepresentetive}
+          />
+          <Detail label="WhatsApp Number" value={team?.phone} />
+          <Detail label="Country" value={team?.country} />
+          <Detail label="Postal Code" value={team?.postalcode} />
+        </VStack>
+      </VStack>
     </Box>
   );
 };
--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -81,6 +81,72 @@ const Documents = () => {
                 dateTime="2024-09-22T12:22:00Z"
                 color={useColorModeValue("gray.500", "gray.400")}
               >
+                {timeDifference("2024-10-28T12:22:00Z")}
+              </Box>
+              <Heading size="md" my={2}>
+                <LinkOverlay
+                  href="https://ghc-document.s3.ap-south-1.amazonaws.com/PDS_Guidelines.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  color={useColorModeValue("teal.500", "teal.300")}
+                >
+                  PDS Guidelines (DesignX Blueprint)
+                </LinkOverlay>
+              </Heading>
+              <Text color={useColorModeValue("gray.600", "gray.300")}>
+                GLOBAL HYPERLOOP COMPETITION 2025
+              </Text>
+            </LinkBox>
+
+            <LinkBox
+              as="article"
+              maxW="sm"
+              p={5}
+              borderWidth={10}
+              rounded="lg"
+              shadow="md"
+              bg={useColorModeValue("white", "gray.700")}
+              transition="transform 0.2s"
+              _hover={{ transform: "scale(1.05)" }}
+            >
+              <Box
+                as="time"
+                dateTime="2024-09-22T12:22:00Z"
+                color={useColorModeValue("gray.500", "gray.400")}
+              >
+                {timeDifference("2024-10-28T12:22:00Z")}
+              </Box>
+              <Heading size="md" my={2}>
+                <LinkOverlay
+                  href="https://ghc-document.s3.ap-south-1.amazonaws.com/SPD_Guidelines(Innoquest).pdf "
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  color={useColorModeValue("teal.500", "teal.300")}
+                >
+                  SPD Guidelines (Innoquest)
+                </LinkOverlay>
+              </Heading>
+              <Text color={useColorModeValue("gray.600", "gray.300")}>
+                GLOBAL HYPERLOOP COMPETITION 2025
+              </Text>
+            </LinkBox>
+
+            <LinkBox
+              as="article"
+              maxW="sm"
+              p={5}
+              borderWidth={10}
+              rounded="lg"
+              shadow="md"
+              bg={useColorModeValue("white", "gray.700")}
+              transition="transform 0.2s"
+              _hover={{ transform: "scale(1.05)" }}
+            >
+              <Box
+                as="time"
+                dateTime="2024-09-22T12:22:00Z"
+                color={useColorModeValue("gray.500", "gray.400")}
+              >
                 {timeDifference("2024-09-27T12:22:00Z")}
               </Box>
               <Heading size="md" my={2}>
@@ -90,7 +156,7 @@ const Documents = () => {
                   rel="noopener noreferrer"
                   color={useColorModeValue("teal.500", "teal.300")}
                 >
-                Innoquest Problem Statements
+                  Innoquest Problem Statements
                 </LinkOverlay>
               </Heading>
               <Text color={useColorModeValue("gray.600", "gray.300")}>
