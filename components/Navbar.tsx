31df7ad944e97c7ec8e6e38f18bb9033db668dc0
Global Hyperloop Competition <ghc@smail.iitm.ac.in>
2025-10-10 00:36:34 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -156,7 +156,7 @@ export default function WithSubnavigation() {
                     </motion.div>
                   </>
                 ) : (
-                  <Link href={""}>
+                  <Link href={"https://drive.google.com/file/d/1r0FueeTb0TAoBeNUBSzViHbUBH-L0I9v/view"}>
                     <motion.div
                       whileTap={{ scale: 0.95 }}
                       whileHover={{ scale: 1.05 }}
