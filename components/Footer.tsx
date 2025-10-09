95306c51a73a4f79a9709a24498d2a276ce75f6b
Global Hyperloop Competition <ghc@smail.iitm.ac.in>
2025-10-09 22:59:50 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -108,7 +108,7 @@ export default function LargeWithNewsletter() {
               </Link>
             </HStack>
             <Text fontSize={"sm"}>© 2025 GHC . All rights reserved</Text>
-            <Stack direction={"row"} spacing={56}>
+            <Stack direction={"row"} spacing={6}>
               <SocialButton
                 label={"Instagram"}
                 href="https://www.instagram.com/ghc_india/"
