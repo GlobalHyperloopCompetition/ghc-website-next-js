6e87edcfbf20d5616521367a437ee0fa11b0bd59
Global Hyperloop Competition <ghc@smail.iitm.ac.in>
2025-10-30 03:53:35 +0530

    Update HomeHero.tsx

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -72,7 +72,7 @@ export default function HomeHero() {
           <Stack mt={6} direction={{ base: "column", sm: "row" }}>
             <motion.div whileTap={{ scale: 0.95 }} whileHover={{ scale: 1.05 }}>
               Take a look at {"   "}
-              <Link href="/gallery" passHref>
+              <Link href="https://forms.gle/ENnNUHNLrJNDdLuB7" passHref>
                 <Button
                   rounded="xl"
                   size="lg"
